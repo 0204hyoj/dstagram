4915efeefc14b8334591eea3dc2cf03121b03002
0204hyoj <89888712+0204hyoj@users.noreply.github.com>
2023-04-26 12:08:39 +0900

    Dstgram Service

--- photo/views.py
+++ photo/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Photo
+from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.detail import DetailView
+from django.contrib.auth import get_user_model
+
 
 
 # Create your views here.
-@login_required
-def photo_list(request):
-    photos = Photo.objects.all()
-    return render(request, 'photo/list.html', {'photos': photos})
+class PhotoList(LoginRequiredMixin, ListView):
+    model = Photo
+    template_name = 'photo/list.html'
+    context_object_name = 'photos'
+    def photo_list(request):
+        photos = Photo.objects.all()  # 데이터베이스에 저장 된 모든 사진을 불러온다.
+        return render(request, 'photo/list.html', {'photos': photos})
+
 
 class PhotoUploadView(LoginRequiredMixin, CreateView):
     model = Photo
@@ -33,3 +41,8 @@ class PhotoUpdateView(LoginRequiredMixin, UpdateView):
     model = Photo
     fields = ['photo', 'text']
     template_name = 'photo/update.html'
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'photo/user_detail.html'
+    context_object_name = 'user_object'
